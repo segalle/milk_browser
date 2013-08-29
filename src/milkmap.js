02336ba07def73d8d9a42b159a2d243d1376c5c3
Erez Segall <erez.segall@gmail.com>
2013-08-29 10:56:32 +0300

    added current location zoom
    fixed relative view problem

--- src/milkmap.js
+++ src/milkmap.js
@@ -4,13 +4,40 @@ function initialize() {
 	var mapOptions = {
 		zoom: 7,
       	center: new google.maps.LatLng(31.046051,34.851612),
-      	mapTypeId: google.maps.MapTypeId.TERRAIN
+      	mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     map = new google.maps.Map(document.getElementById('map_canvas'),
         mapOptions); 
     var script = document.createElement('script');
     script.src = 'milk.geojson';
     document.getElementsByTagName('head')[0].appendChild(script);
+    
+    if(navigator.geolocation) {
+    	browserSupportFlag = true;
+    	navigator.geolocation.getCurrentPosition(function(position) {
+      		initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+      		map.setCenter(initialLocation);
+      		map.setZoom(17);
+    }, function() {
+      handleNoGeolocation(browserSupportFlag);
+    });
+  }
+  // Browser doesn't support Geolocation
+  else {
+    browserSupportFlag = false;
+    handleNoGeolocation(browserSupportFlag);
+  }
+  
+  function handleNoGeolocation(errorFlag) {
+    if (errorFlag == true) {
+      alert("Geolocation service failed.");
+      initialLocation = newyork;
+    } else {
+      alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
+      initialLocation = siberia;
+    }
+    map.setCenter(initialLocation);
+  }
   }
 
 window.eqfeed_callback = function(results) {
