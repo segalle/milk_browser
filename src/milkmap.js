b83b60d7f57b88d62c6f04fccc8ed87f4b0816d3
Udi Oron <udioron@gmail.com>
2013-08-29 11:41:09 +0300

    modified geojson loading to use jquery and ajax.

--- src/milkmap.js
+++ src/milkmap.js
@@ -1,15 +1,56 @@
 var map;
 
+var Milk = {};
+
+
+var goldStar = {
+    path : 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
+    fillColor : "yellow",
+    fillOpacity : 0.8,
+    scale : 0.1,
+    strokeColor : "gold",
+    strokeWeight : 14
+};
+
+var showPosition = function(position) {
+    var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    var marker = new google.maps.Marker({
+        icon : goldStar,
+        position : userLatLng,
+        title : 'Your Location',
+        map : map
+    });
+};
+
+function processGeoJSON(results) {
+
+    Milk.geojson = results;
+
+    var markers = [];
+    for (var i = 0; i < results.features.length; i++) {
+        var coords = results.features[i].geometry.coordinates;
+        var latLng = new google.maps.LatLng(coords[1], coords[0]);
+        var marker = new google.maps.Marker({
+            position : latLng,
+            map : map
+        });
+        markers.push(marker);
+    }
+    var markerCluster = new MarkerClusterer(map, markers);
+
+};
+
 function initialize() {
+
     var mapOptions = {
         zoom : 7,
         center : new google.maps.LatLng(31.046051, 34.851612),
         mapTypeId : google.maps.MapTypeId.ROADMAP
     };
+
     map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-    var script = document.createElement('script');
-    script.src = 'milk.geojson';
-    document.getElementsByTagName('head')[0].appendChild(script);
+
+    $.get('milk.geojson', {}, processGeoJSON, 'json');
 
     if (navigator.geolocation) {
         browserSupportFlag = true;
@@ -31,6 +72,7 @@ function initialize() {
     }
 
     function handleNoGeolocation(errorFlag) {
+        // TODO: maybe nothing
         if (errorFlag == true) {
             alert("Geolocation service failed.");
             initialLocation = newyork;
@@ -43,35 +85,5 @@ function initialize() {
 
 }
 
-var goldStar = {
-    path : 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
-    fillColor : "yellow",
-    fillOpacity : 0.8,
-    scale : 0.1,
-    strokeColor : "gold",
-    strokeWeight : 14
-};
 
-var showPosition = function(position) {
-    var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    var marker = new google.maps.Marker({
-        icon : goldStar,
-        position : userLatLng,
-        title : 'Your Location',
-        map : map
-    });
-};
-
-window.eqfeed_callback = function(results) {
-    var markers = [];
-    for (var i = 0; i < results.features.length; i++) {
-        var coords = results.features[i].geometry.coordinates;
-        var latLng = new google.maps.LatLng(coords[1], coords[0]);
-        var marker = new google.maps.Marker({
-            position : latLng,
-            map : map
-        });
-        markers.push(marker);
-    }
-    var markerCluster = new MarkerClusterer(map, markers);
-};
+$(initialize);
