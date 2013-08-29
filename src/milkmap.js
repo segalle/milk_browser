8ffd49fb785e924667e77dba531510c6c0075b4a
Erez Segall <erez.segall@gmail.com>
2013-08-29 14:25:46 +0300

    added infowindow

--- src/milkmap.js
+++ src/milkmap.js
@@ -49,8 +49,46 @@ function initialize() {
     };
 
     map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+	//------------------------------------------
+	$.get('milk.geojson', {}, processGeoJSON, 'json');
+	  
+	  
+	  var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+      '<div id="bodyContent">'+
+      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+      'sandstone rock formation in the southern part of the '+
+      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+      'south west of the nearest large town, Alice Springs; 450&#160;km '+
+      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+      'Aboriginal people of the area. It has many springs, waterholes, '+
+      'rock caves and ancient paintings. Uluru is listed as a World '+
+      'Heritage Site.</p>'+
+      '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+      'http://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+      '(last visited June 22, 2009).</p>'+
+      '</div>'+
+      '</div>';
 
-    $.get('milk.geojson', {}, processGeoJSON, 'json');
+  var infowindow = new google.maps.InfoWindow({
+      content: contentString
+  });
+  // var marker = new google.maps.Marker({
+      // position: myLatlng,
+      // map: map,
+      // title: 'Uluru (Ayers Rock)'
+  // });
+  
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
+	});
+	
+	//------------------------------------------
+
 
     if (navigator.geolocation) {
         browserSupportFlag = true;
@@ -82,7 +120,6 @@ function initialize() {
         }
         map.setCenter(initialLocation);
     }
-
 }
 
 
