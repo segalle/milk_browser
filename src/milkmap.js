a5434af91abf742841a8c69cce553ee3b959b39e
Erez Segall <erez.segall@gmail.com>
2013-08-29 10:01:17 +0300

    added clustered markers

--- src/milkmap.js
+++ src/milkmap.js
@@ -1,27 +1,26 @@
 var map;
 
-  function initialize() {
-    var mapOptions = {
-      zoom: 2,
-      center: new google.maps.LatLng(2.8,-187.3),
-      mapTypeId: google.maps.MapTypeId.TERRAIN
+function initialize() {
+	var mapOptions = {
+		zoom: 7,
+      	center: new google.maps.LatLng(31.046051,34.851612),
+      	mapTypeId: google.maps.MapTypeId.TERRAIN
     };
     map = new google.maps.Map(document.getElementById('map_canvas'),
-          mapOptions);
-
+        mapOptions); 
     var script = document.createElement('script');
     script.src = 'milk.geojson';
     document.getElementsByTagName('head')[0].appendChild(script);
   }
 
-  window.eqfeed_callback = function(results) {
-    for (var i = 0; i < results.features.length; i++) {
-      var coords = results.features[i].geometry.coordinates;
-      var latLng = new google.maps.LatLng(coords[1],coords[0]);
-      var marker = new google.maps.Marker({
-        position: latLng,
-        map: map
-      });
+window.eqfeed_callback = function(results) {
+	var markers = [];
+	for (var i = 0; i < results.features.length; i++) {
+		var coords = results.features[i].geometry.coordinates;
+		var latLng = new google.maps.LatLng(coords[1],coords[0]);
+		var marker = new google.maps.Marker({position: latLng, map: map});
+        markers.push(marker);
     }
-  }
+    var markerCluster = new MarkerClusterer(map, markers);
+  };
  
